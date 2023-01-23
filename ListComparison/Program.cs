a7c0bfed9d59d37dc9bf2ff0d4ff135a7039d2de
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-01-23 21:32:37 +0800

    ListComparison

--- ListComparison/Program.cs
+++ ListComparison/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+namespace ListComparison;
+
+internal class Program
+{
+   private static void Main(string[] args)
+   {
+      string text = File.ReadAllText(@"C:/Programming/csproj/CsHomeworkCollections/Book.txt");
+      string[] words = text.Split();
+
+      Stopwatch listStopwatch = new();
+      listStopwatch.Start();
+
+      List<string> listedWords = new();
+      foreach (string word in words) {
+         listedWords.Add(word);
+      }
+
+      listStopwatch.Stop();
+
+      Stopwatch linkedStopwatch = new();
+      linkedStopwatch.Start();
+
+      LinkedList<string> linkedWords = new();
+      foreach (string word in words) {
+         linkedWords.AddLast(word);
+      }
+
+      linkedStopwatch.Stop();
+
+      TimeSpan listTimespan = listStopwatch.Elapsed;
+
+      string listTime = string.Format("{0:00}:{1:00}.{2:00}",
+         listTimespan.Minutes,
+         listTimespan.Seconds,
+         listTimespan.Milliseconds
+      );
+      Console.WriteLine(listTime); // 2 ms
+
+      TimeSpan linkedTimespan = linkedStopwatch.Elapsed;
+
+      string linkedTime = string.Format("{0:00}:{1:00}.{2:00}",
+         linkedTimespan.Minutes,
+         linkedTimespan.Seconds,
+         linkedTimespan.Milliseconds
+      );
+      Console.WriteLine(linkedTime); // 20 ms
+
+      // LinkedList медленнее List
+   }
+}
