4d87aedb638a12b6c7cf273f9907579dd43df5ec
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-01-23 21:57:37 +0800

    TopTenWords

---
+++ TopTenWords/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace TopTenWords;
+
+internal class Program
+{
+   private static void Main(string[] args)
+   {
+      string text = File.ReadAllText(@"C:/Programming/csproj/CsHomeworkCollections/Book.txt");
+
+      string noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+
+      Console.OutputEncoding = Encoding.Unicode;
+
+      var orderedWords = noPunctuationText
+      .Split()
+      .GroupBy(x => x)
+      .Select(x => new {
+         KeyField = x.Key,
+         Count = x.Count()
+      })
+      .OrderByDescending(x => x.Count)
+      .Take(10);
+
+      foreach (var word in orderedWords) {
+         Console.WriteLine(word);
+      }
+   }
+}
